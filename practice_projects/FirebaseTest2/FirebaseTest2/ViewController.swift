bb41ea427593a36f1785f4b23025213a34803112
KyusokLee <kyusok1@naver.com>
2022-07-06 23:23:23 +0900

    FirebaseTest2 _ SnapKit Practice

--- practice_projects/FirebaseTest2/FirebaseTest2/ViewController.swift
+++ practice_projects/FirebaseTest2/FirebaseTest2/ViewController.swift
@@ -6,12 +6,64 @@
 //
 
 import UIKit
+import SnapKit
+
+//Firebase 練習
+//SnapKit 練習
+// コードでUIを実装した方法
+// SnapKitを利用すると、AutoLayout設定に関するコードが簡潔になる
+
 
 class ViewController: UIViewController {
 
+    let nameLabel = UILabel()
+    let nameTextField = UITextField()
+    let changeButton = UIButton()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setUpValue()
+        setUpView()
+        setConstraints()
+
+    }
+    
+    //ViewをTapすることで、keyboard dismissする
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // 요소 내용 설정
+    func setUpValue() {
+        nameLabel.text = "Label"
+        nameTextField.backgroundColor = .gray
+        changeButton.backgroundColor = .blue
+    }
+    
+    // ViewのUIセッティング
+    func setUpView() {
+        view.addSubview(nameLabel)
+        view.addSubview(nameTextField)
+        view.addSubview(changeButton)
+    }
+    
+    // ViewのUIのConstraint（制約）設定
+    func setConstraints() {
+        changeButton.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            // Viewの真ん中に配置する
+        }
+            
+        nameLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(80)
+            make.leading.equalToSuperview().offset(24)
+            make.trailing.equalToSuperview().offset(-24)
+        }
+        
+        nameTextField.snp.makeConstraints { make in
+            make.top.equalTo(nameLabel.snp.bottom).offset(24)
+            make.leading.trailing.equalTo(nameLabel)
+        }
     }
 
 
