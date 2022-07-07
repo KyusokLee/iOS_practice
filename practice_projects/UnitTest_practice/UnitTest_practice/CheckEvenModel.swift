6613b011701d6164f73df01a11ca7baafdd7c1a3
KyusokLee <kyusok1@naver.com>
2022-07-07 23:52:36 +0900

    Unit Test_ テストコードの練習1

---
+++ practice_projects/UnitTest_practice/UnitTest_practice/CheckEvenModel.swift
@@ -0,0 +1,8 @@
+//
+//  CheckEvenModel.swift
+//  UnitTest_practice
+//
+//  Created by Kyus'lee on 2022/07/07.
+//
+
+import Foundation
---
+++ practice_projects/UnitTest_practice/UnitTest_practiceUITests/UnitTest_practiceUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  UnitTest_practiceUITestsLaunchTests.swift
+//  UnitTest_practiceUITests
+//
+//  Created by Kyus'lee on 2022/07/07.
+//
+
+import XCTest
+
+class UnitTest_practiceUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
