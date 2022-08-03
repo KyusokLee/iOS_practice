290e2c83df550aa1038e3d9e1580e8d25d53a528
KyusokLee <kyusok1@naver.com>
2022-08-03 17:58:59 +0900

    Calculator_ app updated

--- practice_projects/Calculator_prac/Calculator_prac/ViewController.swift
+++ practice_projects/Calculator_prac/Calculator_prac/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 // Groupを分けて、見やすい(均等なUIを表示させたい場合)UIを表示させたいとき、まず最初にUIViewで括ってから、中にobjectを配置するとより調整しやすくなる (方法1)
 
+// UIViewの上に、buttonを載せて、均等な位置配置を行いたいbuttonをstackViewでくくる
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
